f2583d35202578a4df035006b04eacd92a40d08b
Andres <arenasandres7@gmail.com>
2026-01-23 16:45:34 -0500

    update module7

--- main.tf
+++ main.tf
@@ -1,17 +1,20 @@
 ###Meticas para cada una de las instancias EC2.
 resource "aws_cloudwatch_metric_alarm" "ec2_cpu_utilization_high" {
   for_each            = var.ec2_instances
-  alarm_name          = "EC2_CPU_Utilization_High_-${var.project}-${var.environment}-${each.key}"
+  alarm_name          = "${var.project}-${var.name_service}-CPUUtilization-${each.key}-${var.environment}"
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 2
+  evaluation_periods  = var.cpu_utilization_evaluation_periods
   metric_name         = "CPUUtilization"
   namespace           = "AWS/EC2"
-  period              = "300"
+  period              = var.cpu_utilization_period
   statistic           = "Average"
   threshold           = var.cpu_utilization_threshold
-  datapoints_to_alarm = 2
   treat_missing_data  = "notBreaching"
   alarm_description   = "Alarm when CPU utilization exceeds"
+
+  tags = merge({
+    name = "${var.project}-${var.name_service}-CPUUtilization-${each.key}-${var.environment}"
+  })
 
   dimensions = {
     InstanceId = each.key
@@ -20,20 +23,20 @@ resource "aws_cloudwatch_metric_alarm" "ec2_cpu_utilization_high" {
 
 resource "aws_cloudwatch_metric_alarm" "ec2_memory_utilization_high" {
   for_each            = var.ec2_instances
-  alarm_name          = "EC2_Memory_Utilization_High-${var.project}-${var.environment}-${each.key}"
+  alarm_name          = "${var.project}-${var.name_service}-MemoryUtilization-${each.key}-${var.environment}"
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 2
-  datapoints_to_alarm = 2
+  evaluation_periods  = var.memory_utilization_threshold_evaluation_periods
+  metric_name         = "mem_used_percent"
+  namespace           = "CWAgent"
+  period              = var.memory_utilization_period
+  statistic           = "Average"
+  threshold           = var.memory_utilization_threshold
+  treat_missing_data  = "notBreaching"
+  alarm_description   = "Memory utilization > ${var.memory_utilization_threshold}%"
 
-  metric_name = "mem_used_percent"
-  namespace   = "CWAgent"
-  period      = 300
-  statistic   = "Average"
-  threshold   = var.memory_utilization_threshold
-
-  treat_missing_data = "notBreaching"
-
-  alarm_description = "Memory utilization > ${var.memory_utilization_threshold}%"
+  tags = merge({
+    name = "${var.project}-${var.name_service}-MemoryUtilization-${each.key}-${var.environment}"
+  })
 
   dimensions = {
     InstanceId = each.key
@@ -42,20 +45,20 @@ resource "aws_cloudwatch_metric_alarm" "ec2_memory_utilization_high" {
 
 resource "aws_cloudwatch_metric_alarm" "ec2_disk_utilization_high" {
   for_each            = var.ec2_instances
-  alarm_name          = "EC2_Disk_Utilization_High-${var.project}-${var.environment}-${each.key}"
+  alarm_name          = "${var.project}-${var.name_service}-DiskUtilization-${each.key}-${var.environment}"
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 2
-  datapoints_to_alarm = 2
+  evaluation_periods  = var.disk_utilization_threshold_evaluation_periods
+  metric_name         = "disk_used_percent"
+  namespace           = "CWAgent"
+  period              = var.disk_utilization_period
+  statistic           = "Average"
+  threshold           = var.disk_utilization_threshold
+  treat_missing_data  = "notBreaching"
+  alarm_description   = "Disk utilization > ${var.disk_utilization_threshold}%"
 
-  metric_name = "disk_used_percent"
-  namespace   = "CWAgent"
-  period      = 300
-  statistic   = "Average"
-  threshold   = var.disk_utilization_threshold
-
-  treat_missing_data = "notBreaching"
-
-  alarm_description = "Disk utilization > ${var.disk_utilization_threshold}%"
+  tags = merge({
+    name = "${var.project}-${var.name_service}-DiskUtilization-${each.key}-${var.environment}"
+  })
 
   dimensions = {
     InstanceId = each.key
--- variables.tf
+++ variables.tf
@@ -11,11 +11,10 @@ variable "sns_topic_arn" {
   type        = list(string)
 }
 
-
 variable "project" {
   description = "Nombre del proyecto"
   type        = string
-  default     = "Monitioreo"
+  default     = "Andres-Monitioreo"
 }
 
 variable "environment" {
@@ -24,23 +23,71 @@ variable "environment" {
   default     = "Dev"
 }
 
-###Variables de alerta
+variable "name_service" {
+  description = "Nombre del servicio"
+  type        = string
+  default     = "EC2"
+}
+
+variable "purpose" {
+  description = "Propósito del recurso"
+  type        = string
+  default     = "Monitoring"
+}
+
+########################cpu_utilization_high##########
+variable "cpu_utilization_evaluation_periods" {
+  description = "Número de períodos de evaluación para la alarma de alta utilización de CPU"
+  type        = number
+  default     = 2 # 2 períodos de evaluación
+}
+
+variable "cpu_utilization_period" {
+  description = "Período de la métrica en segundos para la alarma de alta utilización de CPU"
+  type        = number
+  default     = 60 # 60 segundos (1 minuto)  
+}
+
 variable "cpu_utilization_threshold" {
   description = "Umbral de utilización de CPU para activar la alarma"
   type        = number
-  default     = 30
+  default     = 60 # 60% de utilización de CPU
 }
 
-###Variable de umbral de memoria
+########################memory_utilization_high##########
+variable "memory_utilization_threshold_evaluation_periods" {
+  description = "Número de períodos de evaluación para la alarma de alta utilización de memoria"
+  type        = number
+  default     = 2 # 2 períodos de evaluación
+}
+
+variable "memory_utilization_period" {
+  description = "Período de la métrica en segundos para la alarma de alta utilización de memoria"
+  type        = number
+  default     = 60 # 60 segundos (1 minuto)  
+}
+
 variable "memory_utilization_threshold" {
   description = "Umbral de utilización de memoria para activar la alarma"
   type        = number
-  default     = 20
+  default     = 80 # 80% de utilización de memoria
 }
 
-###Variable de umbral de disco
+########################disk_utilization##########
+variable "disk_utilization_threshold_evaluation_periods" {
+  description = "Número de períodos de evaluación para la alarma de alta utilización de disco"
+  type        = number
+  default     = 2 # 2 períodos de evaluación
+}
+
+variable "disk_utilization_period" {
+  description = "Período de la métrica en segundos para la alarma de alta utilización de disco"
+  type        = number
+  default     = 60 # 60 segundos (1 minuto)  
+}
+
 variable "disk_utilization_threshold" {
   description = "Umbral de utilización de disco para activar la alarma"
   type        = number
-  default     = 10
+  default     = 85 # 85% de utilización de disco
 }
