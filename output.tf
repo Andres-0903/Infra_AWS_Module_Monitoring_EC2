2c523d0b8b5bed5e6b6200c8ba4361dfb3a274ff
Andres <arenasandres7@gmail.com>
2026-01-27 15:00:22 -0500

    update module3

--- output.tf
+++ output.tf
@@ -1,8 +1,11 @@
-output "all_alarm_arns" {
-  description = "ARNs de todas las alarmas EC2 creadas"
-  value = concat(
-    [for a in aws_cloudwatch_metric_alarm.ec2_cpu_utilization_high : a.arn],
-    [for a in aws_cloudwatch_metric_alarm.ec2_memory_utilization_high : a.arn],
-    [for a in aws_cloudwatch_metric_alarm.ec2_disk_utilization_high : a.arn]
-  )
+output "ec2_metrics" {
+  value = {
+    for key in keys(var.ec2_instances) : key => {
+      cpu_alarm_arn    = aws_cloudwatch_metric_alarm.ec2_cpu_utilization_high[key].arn
+      memory_alarm_arn = aws_cloudwatch_metric_alarm.ec2_memory_utilization_high[key].arn
+      disk_alarm_arn   = aws_cloudwatch_metric_alarm.ec2_disk_utilization_high[key].arn
+    }
+  }
+  description = "ARNs of CloudWatch Alarms for EC2 instances"
+
 }
